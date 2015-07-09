2fd8851dd642dc29659d934b99709bf7a8da4f00
marknagelberg <nagelbergmark@gmail.com>
2015-07-08 22:34:46 -0500

    Committing to be able to go back to refactored code

--- benchmark.py
+++ benchmark.py
@@ -21,8 +21,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import text
 from sklearn.cross_validation import KFold, StratifiedKFold
 
-def extract_features(data, stemmed):
-    token_pattern = re.compile(r"(?u)\b\w\w+\b")
+
+
+def extract_features(data):
+    '''
+    Perform feature extraction for variables that can be extracted
+    the same way for both training and test data sets. The input
+    "data" is the pandas dataframe for the training or test sets.
+    '''
+    token_pattern = re.compile(r"(?u)\b\w+\b")
     data["query_tokens_in_title"] = 0.0
     data["query_tokens_in_description"] = 0.0
     data["percent_query_tokens_in_description"] = 0.0
@@ -40,35 +47,6 @@ def extract_features(data, stemmed):
         data.set_value(i, "query_length", len(query))
         data.set_value(i, "description_length", len(description))
         data.set_value(i, "title_length", len(title))
-        exact_query_in_title = 0
-        if row["query"].lower() in row["product_title"].lower():
-            exact_query_in_title = 1
-        data.set_value(i, "exact_query_in_title", exact_query_in_title)
-        exact_query_in_description = 0
-        if row["query"].lower() in row["product_description"].lower():
-            exact_query_in_description = 1
-        data.set_value(i, "exact_query_in_description", exact_query_in_description)
-
-        if stemmed:
-            translate_to = u''
-            translate_table = dict((ord(char), translate_to) for char in u' -')
-            q_space_removed = row["query"].lower().translate(translate_table)
-            t_space_removed = row["product_title"].lower().translate(translate_table)
-            d_space_removed = row["product_description"].lower().translate(translate_table)
-        if not stemmed:
-            q_space_removed = row["query"].lower().translate(None, ' -')
-            t_space_removed = row["product_title"].lower().translate(None, ' -')
-            d_space_removed = row["product_description"].lower().translate(None, ' -')
-
-        if q_space_removed in t_space_removed:
-            data.set_value(i, "space_removed_q_in_t", 1)
-        else:
-            data.set_value(i, "space_removed_q_in_t", 0)
-
-        if q_space_removed in d_space_removed:
-            data.set_value(i, "space_removed_q_in_d", 1)
-        else:
-            data.set_value(i, "space_removed_q_in_d", 0)
 
         two_grams_in_query = set(get_n_grams(row["query"], 2))
         two_grams_in_title = set(get_n_grams(row["product_title"], 2))
@@ -78,7 +56,7 @@ def extract_features(data, stemmed):
         data.set_value(i, "two_grams_in_q_and_d", len(two_grams_in_query.intersection(two_grams_in_description)))
 
 def get_string_similarity(s1, s2):
-    token_pattern = re.compile(r"(?u)\b\w\w+\b")
+    token_pattern = re.compile(r"(?u)\b\w+\b")
     s1_tokens = set(x.lower() for x in token_pattern.findall(s1))
     s2_tokens = set(x.lower() for x in token_pattern.findall(s2))
     if len(s1_tokens.union(s2_tokens)) == 0:
@@ -87,6 +65,12 @@ def get_string_similarity(s1, s2):
         return float(len(s1_tokens.intersection(s2_tokens)))/float(len(s1_tokens.union(s2_tokens)))
 
 def get_n_gram_string_similarity(s1, s2, n):
+    '''
+    Helper function to get the n-gram "similarity" between two strings,
+    where n-gram similarity is defined as the percentage of n-grams
+    the two strings have in common out of all of the n-grams across the
+    two strings.
+    '''
     s1 = set(get_n_grams(s1, n))
     s2 = set(get_n_grams(s2, n))
     if len(s1.union(s2)) == 0:
@@ -96,7 +80,7 @@ def get_n_gram_string_similarity(s1, s2, n):
 
 def get_n_grams(s, n):
     '''
-    Takes in a string and the degree of n gram n and returns a list of all the
+    Helper function that takes in a string and the degree of n gram n and returns a list of all the
     n grams in the string. String is separated by space.
     '''
     word_list = s.split()
@@ -112,79 +96,32 @@ def get_n_grams(s, n):
             n_grams.append(tuple(n_gram))
     return n_grams
 
-def calculate_nearby_relevance_tuple(group, row, col_name):
+def calculate_nearby_relevance_tuple(group, row, col_name, ngrams):
     '''
-    Takes a group of rows for a particular query and a row within that 
-    group and returns several calculations based on the median relevance of
-    "similar" entries. Returns a tuple of calculations. Tuple returns a rating that
-    weights similarity across all other rows and returns a rating that is simply the 
-    rating of the one "closest" row.
+    Takes the group of rows for a particular query ("group") and a row within that 
+    group ("row") and returns a dictionary of "similarity"  calculations of row compared to the rest 
+    of the rows in group. Returns a tuple of calculations that will be used to create similarity features for row.
     '''
-    return_rating = 0
-    return_2gram_rating = 0
-    max_similarity = 0
-    max_2gram_similarity = 0
-    weighted_ratings = {x:[0,0] for x in range(1,5)}
-    weighted_2gram_ratings = {x:[0,0] for x in range(1,5)}
+
+    ngrams = range(1, ngrams + 1)
+    #Weighted ratings takes the form
+    #{median rating : {ngram : [number of comparisons with that rating/ngram, cumulative sum of similarity for that rating/ngram]}}
+    weighted_ratings = {rating: {ngram: [0,0] for ngram in ngrams} for rating in range(1,5)}
+
     for i, group_row in group.iterrows():
-        #Weighted ratings takes the form {median relevance value: [number of comparisons with that relevance value, cumulative sum of similarity]}
-        #We ultimately want to return the median relevance value with the highest cumulative_sum/num_comparisons
         if group_row['id'] != row['id']:
-            similarity = get_string_similarity(row[col_name], group_row[col_name])
-            twogram_similarity = get_n_gram_string_similarity(row[col_name], group_row[col_name], 2)
-            weighted_ratings[group_row['median_relevance']][0] += 1
-            weighted_2gram_ratings[group_row['median_relevance']][0] += 1
-            weighted_ratings[group_row['median_relevance']][1] += similarity
-            weighted_2gram_ratings[group_row['median_relevance']][1] += twogram_similarity
-            if similarity > max_similarity:
-                max_similarity = similarity
-                return_rating = group_row['median_relevance']
-            if twogram_similarity > max_2gram_similarity:
-                max_2gram_similarity = twogram_similarity
-                return_2gram_rating = group_row['median_relevance']
 
+            for ngram in ngrams:
+                similarity = helper.get_n_gram_string_similarity(row[col_name], group_row[col_name], ngram)
+                weighted_ratings[group_row['median_relevance']][ngram][1] += similarity
+                weighted_ratings[group_row['median_relevance']][ngram][0] += 1
 
-    max_weighted_similarity = 0
-    max_weighted_median_rating = 0
-    for rating in weighted_ratings:
-        if weighted_ratings[rating][0] != 0:
-            current_weighted_similarity = float(weighted_ratings[rating][1])/float(weighted_ratings[rating][0])
-            if current_weighted_similarity > max_weighted_similarity:
-                max_weighted_similarity = current_weighted_similarity
-                max_weighted_median_rating = rating
+    return weighted_ratings
 
-    #weighted_median_rating = float(sum(x * weighted_ratings[x][1] for x in weighted_ratings))/float(sum(weighted_ratings[x][0] for x in weighted_ratings))
-    weighted_median_rating = 0
-    for x in weighted_ratings:
-        if float(weighted_ratings[x][0]) != 0.0:
-            weighted_median_rating += (x * weighted_ratings[x][1])/float(weighted_ratings[x][0])
-
-    max_weighted_2gram_similarity = 0
-    max_weighted_2gram_median_rating = 0
-    for rating in weighted_2gram_ratings:
-        if weighted_2gram_ratings[rating][0] != 0:
-            current_weighted_2gram_similarity = float(weighted_2gram_ratings[rating][1])/float(weighted_2gram_ratings[rating][0])
-            if current_weighted_2gram_similarity > max_weighted_2gram_similarity:
-                max_weighted_2gram_similarity = current_weighted_2gram_similarity
-                max_weighted_2gram_median_rating = rating
-
-    #weighted_2gram_median_rating = float(sum(x * weighted_2gram_ratings[x][1] for x in weighted_2gram_ratings))/float(sum(weighted_2gram_ratings[x][0] for x in weighted_2gram_ratings))
-    weighted_2gram_median_rating = 0
-    for x in weighted_2gram_ratings:
-        if float(weighted_2gram_ratings[x][0]) != 0.0:
-            weighted_2gram_median_rating += (x * weighted_2gram_ratings[x][1])/float(weighted_2gram_ratings[x][0])
-
-    return (return_rating, max_weighted_median_rating, weighted_median_rating, return_2gram_rating, max_weighted_2gram_median_rating, weighted_2gram_median_rating), (weighted_ratings, weighted_2gram_ratings)
-
-        
 def extract_training_features(train, test):
     train_group = train.groupby('query')
     test["q_mean_of_training_relevance"] = 0.0
     test["q_median_of_training_relevance"] = 0.0
-    test["closest_title_relevance"] = 0
-    test["closest_description_relevance"] = 0
-    test["closest_2gram_title_relevance"] = 0
-    test["closest_2gram_description_relevance"] = 0
     test["avg_relevance_variance"] = 0
     for i, row in train.iterrows():
         #Move the two blocks below outside this loop - can make them run much faster.
@@ -201,91 +138,50 @@ def extract_training_features(train, test):
         train.set_value(i, "avg_relevance_variance", avg_relevance_variance)
         test.loc[test["query"] == row["query"], "avg_relevance_variance"] = avg_relevance_variance
 
-        (closest_title_relevance, weighted_title_relevance, weighted_title_relevance_two, closest_2gram_title_relevance, weighted_2gram_title_relevance, weighted_2gram_title_relevance_two), (weight_dict, weight_dict_2gram) = calculate_nearby_relevance_tuple(group, row, 'product_title')
-        train.set_value(i, "closest_title_relevance", closest_title_relevance)
-        train.set_value(i, "weighted_title_relevance", weighted_title_relevance)
-        train.set_value(i, "weighted_title_relevance_two", weighted_title_relevance_two)
-        train.set_value(i, "closest_2gram_title_relevance", closest_2gram_title_relevance)
-        train.set_value(i, "weighted_2gram_title_relevance", weighted_2gram_title_relevance)
-        train.set_value(i, "weighted_2gram_title_relevance_two", weighted_2gram_title_relevance_two)
+        weight_dict = calculate_nearby_relevance_tuple(group, row, 'product_title', 3)
+        for rating in weight_dict:
+            for ngram in weight_dict[rating]:
+                variable_name = "average_title_" + str(ngram) + "gram_similarity_" + str(rating)
+                if weight_dict[rating][ngram][0] != 0:
+                    train.set_value(i, variable_name, float(weight_dict[rating][ngram][1])/float(weight_dict[rating][ngram][0]))
+                else:
+                    train.set_value(i, variable_name, 0)
 
-        for j in range(1,5):
-            if weight_dict[j][0] != 0:
-                train.set_value(i, "average_title_similarity_" + str(j), float(weight_dict[j][1])/float(weight_dict[j][0]))
-            else:
-                train.set_value(i, "average_title_similarity_" + str(j), 0)
+        weight_dict = calculate_nearby_relevance_tuple(group, row, 'product_description', 3)
+        for rating in weight_dict:
+            for ngram in weight_dict[rating]:
+                variable_name = "average_description_" + str(ngram) + "gram_similarity_" + str(rating)
+                if weight_dict[rating][ngram][0] != 0:
+                    train.set_value(i, variable_name, float(weight_dict[rating][ngram][1])/float(weight_dict[rating][ngram][0]))
+                else:
+                    train.set_value(i, variable_name, 0)
 
-        for j in range(1,5):
-            if weight_dict_2gram[j][0] != 0:
-                train.set_value(i, "average_title_2gram_similarity_" + str(j), float(weight_dict_2gram[j][1])/float(weight_dict_2gram[j][0]))
-            else:
-                train.set_value(i, "average_title_2gram_similarity_" + str(j), 0)
-
-
-        (closest_description_relevance, weighted_description_relevance, weighted_description_relevance_two, closest_2gram_description_relevance, weighted_2gram_description_relevance, weighted_2gram_description_relevance_two), (weight_dict, weight_dict_2gram) = calculate_nearby_relevance_tuple(group, row, 'product_description')
-        train.set_value(i, "closest_description_relevance", closest_description_relevance)
-        train.set_value(i, "weighted_description_relevance", weighted_description_relevance)
-        train.set_value(i, "weighted_description_relevance_two", weighted_description_relevance_two)
-        train.set_value(i, "closest_2gram_description_relevance", closest_2gram_description_relevance)
-        train.set_value(i, "weighted_2gram_description_relevance", weighted_2gram_description_relevance)
-        train.set_value(i, "weighted_2gram_description_relevance_two", weighted_2gram_description_relevance_two)
-
-        for j in range(1,5):
-            if weight_dict[j][0] != 0:
-                train.set_value(i, "average_description_similarity_" + str(j), float(weight_dict[j][1])/float(weight_dict[j][0]))
-            else:
-                train.set_value(i, "average_description_similarity_" + str(j), 0)
-
-        for j in range(1,5):
-            if weight_dict_2gram[j][0] != 0:
-                train.set_value(i, "average_description_2gram_similarity_" + str(j), float(weight_dict_2gram[j][1])/float(weight_dict_2gram[j][0]))
-            else:
-                train.set_value(i, "average_description_2gram_similarity_" + str(j), 0)
 
     for i, row in test.iterrows():
         group = train_group.get_group(row["query"])
-        (closest_title_relevance, weighted_title_relevance, weighted_title_relevance_two, closest_2gram_title_relevance, weighted_2gram_title_relevance, weighted_2gram_title_relevance_two), (weight_dict, weight_dict_2gram) = calculate_nearby_relevance_tuple(group, row, 'product_title')
-        test.set_value(i, "closest_title_relevance", closest_title_relevance)
-        test.set_value(i, "weighted_title_relevance", weighted_title_relevance)
-        test.set_value(i, "weighted_title_relevance_two", weighted_title_relevance_two)
-        test.set_value(i, "closest_2gram_title_relevance", closest_2gram_title_relevance)
-        test.set_value(i, "weighted_2gram_title_relevance", weighted_2gram_title_relevance)
-        test.set_value(i, "weighted_2gram_title_relevance_two", weighted_2gram_title_relevance_two)
 
-        for j in range(1,5):
-            if weight_dict[j][0] != 0:
-                test.set_value(i, "average_title_similarity_" + str(j), float(weight_dict[j][1])/float(weight_dict[j][0]))
-            else:
-                test.set_value(i, "average_title_similarity_" + str(j), 0)
+        weight_dict = calculate_nearby_relevance_tuple(group, row, 'product_title', 3)
+        for rating in weight_dict:
+            for ngram in weight_dict[rating]:
+                variable_name = "average_title_" + str(ngram) + "gram_similarity_" + str(rating)
+                if weight_dict[rating][ngram][0] != 0:
+                    test.set_value(i, variable_name, float(weight_dict[rating][ngram][1])/float(weight_dict[rating][ngram][0]))
+                else:
+                    test.set_value(i, variable_name, 0)
 
-        for j in range(1,5):
-            if weight_dict_2gram[j][0] != 0:
-                test.set_value(i, "average_title_2gram_similarity_" + str(j), float(weight_dict_2gram[j][1])/float(weight_dict_2gram[j][0]))
-            else:
-                test.set_value(i, "average_title_2gram_similarity_" + str(j), 0)
-
-        (closest_description_relevance, weighted_description_relevance, weighted_description_relevance_two, closest_2gram_description_relevance, weighted_2gram_description_relevance, weighted_2gram_description_relevance_two), (weight_dict, weight_dict_2gram) = calculate_nearby_relevance_tuple(group, row, 'product_description')
-        test.set_value(i, "closest_description_relevance", closest_description_relevance)        
-        test.set_value(i, "weighted_description_relevance", weighted_description_relevance)
-        test.set_value(i, "weighted_description_relevance_two", weighted_description_relevance_two)
-        test.set_value(i, "closest_2gram_description_relevance", closest_2gram_description_relevance)        
-        test.set_value(i, "weighted_2gram_description_relevance", weighted_2gram_description_relevance)
-        test.set_value(i, "weighted_2gram_description_relevance_two", weighted_2gram_description_relevance_two)
-
-        for j in range(1,5):
-            if weight_dict[j][0] != 0:
-                test.set_value(i, "average_description_similarity_" + str(j), float(weight_dict[j][1])/float(weight_dict[j][0]))
-            else:
-                test.set_value(i, "average_description_similarity_" + str(j), 0)
-
-        for j in range(1,5):
-            if weight_dict_2gram[j][0] != 0:
-                test.set_value(i, "average_description_2gram_similarity_" + str(j), float(weight_dict_2gram[j][1])/float(weight_dict_2gram[j][0]))
-            else:
-                test.set_value(i, "average_description_2gram_similarity_" + str(j), 0)
+        weight_dict = calculate_nearby_relevance_tuple(group, row, 'product_description', 3)
+        for rating in weight_dict:
+            for ngram in weight_dict[rating]:
+                variable_name = "average_description_" + str(ngram) + "gram_similarity_" + str(rating)
+                if weight_dict[rating][ngram][0] != 0:
+                    test.set_value(i, variable_name, float(weight_dict[rating][ngram][1])/float(weight_dict[rating][ngram][0]))
+                else:
+                    test.set_value(i, variable_name, 0)
 
 def stem_data(data):
-
+    '''
+    Helper function to stem the raw training and test data.
+    '''
     stemmer = PorterStemmer()
 
     for i, row in data.iterrows():
@@ -307,6 +203,10 @@ def stem_data(data):
         data.set_value(i, "product_description", unicode(d))
 
 def remove_stop_words(data):
+    '''
+    Helper function to remove stop words
+    from the raw training and test data.
+    '''
     stop = stopwords.words('english')
 
     for i, row in data.iterrows():
@@ -362,25 +262,25 @@ def extract_bow_v2_features(train, test, test_contains_labels = False):
 
 def extract(train, test):
 
-    print "Removing stop words in training data"
-    remove_stop_words(train)
-    print "Removing stop words in test data"
-    remove_stop_words(test)
-
-    print "Stemming training data"
-    stem_data(train)
-    print "Stemming test data"
-    stem_data(test)
-
     print "Extracting training features"
-    extract_features(train, stemmed = True)
+    extract_features(train)
     print "Extracting test features"
-    extract_features(test, stemmed = True)
+    extract_features(test)
 
     #Extract features that can only be extracted on the training set
     print "Extracting training/test features"
     extract_training_features(train, test)
 
+    y_train = train.loc[:,"median_relevance"]
+    train.drop("median_relevance", 1)
+
+    if 'median_relevance' in test.columns.values:
+        y_test = test.loc[:, "median_relevance"]
+        test.drop("median_relevance", 1)
+    else:
+        y_test = []
+
+    return train, y_train, test, y_test
 
 if __name__ == '__main__':
 
